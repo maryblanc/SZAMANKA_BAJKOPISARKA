a64059f2b424766eba1e825db589a3d011d50d29
maryblanc <mariablan.racing@gmail.com>
2026-05-13 23:35:40 +0200

    add cozy UI and password screen

--- script.js
+++ script.js
@@ -1,2 +1,46 @@
-document.getElementById("quote").innerText =
-  "skill issue";
+const PASSWORD = "szamanka";
+
+function checkPassword() {
+
+  const input =
+    document
+      .getElementById("passwordInput")
+      .value;
+
+  if(input === PASSWORD) {
+
+    document
+      .getElementById("lockScreen")
+      .style.display = "none";
+
+    document
+      .getElementById("app")
+      .classList.remove("hidden");
+
+    localStorage.setItem("tereUnlocked", "true");
+
+  } else {
+
+    document
+      .getElementById("errorText")
+      .innerText =
+        "wrong password :(";
+  }
+}
+
+window.onload = () => {
+
+  const unlocked =
+    localStorage.getItem("tereUnlocked");
+
+  if(unlocked === "true") {
+
+    document
+      .getElementById("lockScreen")
+      .style.display = "none";
+
+    document
+      .getElementById("app")
+      .classList.remove("hidden");
+  }
+};
